895c76c4400e50776db56b2deac3d4acb84c4360
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-18 20:23:49 +0300

    adding color var for themes

--- src/js/theme.js
+++ src/js/theme.js
@@ -4,28 +4,29 @@ const labelBasic= document.querySelector('.label-basic')
 const switcherLight = document.getElementById('light');
 const labelLight = document.querySelector('.label-light')
 
-const switcherPurple = document.getElementById('purple');
-const labelPurple = document.querySelector('.label-purple')
+const switcherViolet = document.getElementById('violet')
+const labelViolet = document.querySelector('.label-violet')
+
 
 function onSwitch() {
     if(switcherBasic.checked){
         labelLight.classList.remove('slider-btn')
-        labelPurple.classList.remove('slider-btn')
+        labelViolet.classList.remove('slider-btn')
         labelBasic.classList.add('slider-btn')
     }
     if(switcherLight.checked){
         labelBasic.classList.remove('slider-btn')
-        labelPurple.classList.remove('slider-btn')
+        labelViolet.classList.remove('slider-btn')
         labelLight.classList.add('slider-btn')
     }
 
-    if(switcherPurple.checked){
+    if(switcherViolet.checked){
         labelBasic.classList.remove('slider-btn')
         labelLight.classList.remove('slider-btn')
-        labelPurple.classList.add('slider-btn')
+        labelViolet.classList.add('slider-btn')
     }
 }
 
 switcherBasic.addEventListener('change', onSwitch)
 switcherLight.addEventListener('change', onSwitch)
-switcherPurple.addEventListener('change', onSwitch)
+switcherViolet.addEventListener('change', onSwitch)
