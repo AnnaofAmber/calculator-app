6ee30304ee227bce07b524900528c441b62ad31f
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-02 21:41:26 +0300

    adding onMinus function

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,72 +1,88 @@
-const result = document.querySelector('.result')
-const btnNumbers = document.querySelectorAll('.btn-number')
-const btnOperations = document.querySelectorAll('.operation')
-const btnEquals = document.querySelector('.btn-equals')
-const btnReset = document.querySelector('.btn-reset')
-let operands = []
-const secondNumber = []
-let total=0
-let plus = false
-let equals = false
+const result = document.querySelector('.result');
+const btnNumbers = document.querySelectorAll('.btn-number');
+const btnOperations = document.querySelectorAll('.operation');
+const btnEquals = document.querySelector('.btn-equals');
+const btnReset = document.querySelector('.btn-reset');
+let operands = [];
+let numberMinus ;
+let total = 0;
+let plus = false;
+let minus = false;
 
-function onNumberClick(event){
-operands.push(Number(event.currentTarget.textContent))
-console.log(operands);
-result.textContent = event.currentTarget.textContent
-console.log(plus);
-if(plus){
-onPlus()
+function onNumberClick(event) {
+  if (!minus) {
+    numberMinus = Number(event.currentTarget.textContent);
+  } 
+  if(total !== 0){
+    numberMinus = 0
+  }
+
+
+  operands.push(Number(event.currentTarget.textContent));
+  result.textContent = operands.map(e => e).join('');
+  console.log(minus);
+  if (plus) {
+    onPlus();
+  }
+  if (minus) {
+    onMinus();
+  }
 }
-if(equals){
-    operands = []
-}
-}
 
+function onOperationClick(event) {
+  result.textContent = event.currentTarget.textContent;
 
-
-
-function onOperationClick(event){
-    
-    result.textContent = event.currentTarget.textContent
-
-    if(event.currentTarget.textContent==='+'){
-        if (!plus) {
-            onPlus()
-        }
+  if (event.currentTarget.textContent === '+') {
+    if (!plus) {
+      onPlus();
     }
-    operands=[]
+  }
+
+  if (event.currentTarget.textContent === '-') {
+    if (!minus) {
+      onMinus();
+    }
+  }
+  operands = [];
+}
+
+function onPlus() {
+  const numbers = [];
+  numbers.push(operands);
+  plus = true;
+  const sum = numbers.map(e => total + Number(e));
+  total = Number(sum);
+}
+
+function onMinus() {
+  const numbers = [];
+  numbers.push(operands);
+  let nM = numberMinus
+  minus = true;
+  const difference = numbers.map(
+    e => {
+       total = total - Number(e) +nM
+       
+       return total;
+    }
+
+  );
+ 
+  console.log(difference);
+  total = Number(difference);
+}
+
+function onEquals() {
+  result.textContent = total;
 
 }
 
-function onPlus(){
-const numbers = []
-numbers.push(operands)
-console.log(numbers);
-plus = true
+btnNumbers.forEach(number => {
+  number.addEventListener('click', onNumberClick);
+});
 
-const sum = numbers.map(e => {
-    return total+ Number(e)
-})
-console.log(sum);
-total = Number(sum)
-console.log(total);
+btnOperations.forEach(operation => {
+  operation.addEventListener('click', onOperationClick);
+});
 
-}
-
-
-
-function onEquals(){
-    equals = true
-    result.textContent = total
-}
-
-
-btnNumbers.forEach(number=>{
-    number.addEventListener('click', onNumberClick)
-})
-
-btnOperations.forEach(operation=>{
-    operation.addEventListener('click', onOperationClick)
-})
-
-btnEquals.addEventListener('click', onEquals)
+btnEquals.addEventListener('click', onEquals);
