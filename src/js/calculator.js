e49acfdc383c5f5534197b74c975d410e14d84ab
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-26 18:45:30 +0300

    fixing problem with overflow

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -52,7 +52,7 @@ else{
   operands.splice(1,1)  
 }
 
-
+fitText()
 }
 
 function onOperationClick(event) {
@@ -217,7 +217,7 @@ function onEquals() {
   }
   operands = []
   result.textContent = total
-
+  fitText()
 }
 
 function onReset(){
@@ -275,4 +275,38 @@ btnPeriod.addEventListener('click', onPeriod)
 
 btnDelete.addEventListener('click', onDelete)
 
+
+function fitText(){
+  // max font size in pixels
+  const maxFontSize = 56;
+  // get the DOM output element by its selector
+  let outputDiv = result;
+  // get element's width
+  let width = outputDiv.clientWidth;
+  console.log(width);
+  // get content's width
+  let contentWidth = outputDiv.scrollWidth;
+  console.log(contentWidth);
+  // get fontSize
+  let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'), 10);
+  // if content's width is bigger then elements width - overflow
+  if (contentWidth > width){
+      fontSize = Math.ceil(fontSize * width/contentWidth,10);
+      fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
+      outputDiv.style.fontSize = fontSize+'px';   
+  }else{
+      // content is smaller then width... let's resize in 1 px until it fits 
+      while (contentWidth === width && fontSize < maxFontSize){
+          fontSize = Math.ceil(fontSize) + 1;
+          fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
+          outputDiv.style.fontSize = fontSize+'px';   
+          // update widths
+          width = outputDiv.clientWidth;
+          contentWidth = outputDiv.scrollWidth;
+          if (contentWidth > width){
+              outputDiv.style.fontSize = fontSize-1+'px'; 
+          }
+      }
+  }
+}
 
